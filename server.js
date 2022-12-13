22f9537ce0579553ab40c317866d7711e75bf0be
rak10 <ahmadus10@icloud.com>
2022-12-12 23:46:43 -0500

    added route file

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 // imports 
 const express = require('express');
+const fs = require('fs');
+const api = require('./Develop/routes/route');
+const path = require('path');
+const noteData = require('./Develop/db/db.json');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+// MIDDLEWARE 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+ 
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`Express server listening on port ${PORT}!`)
+);
